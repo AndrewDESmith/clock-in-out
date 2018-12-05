6f492ec9a39cd010b0085b9e56cbb5e0798f9525
Andrew Smith <andrewsmith@Andrews-iMac.local>
2018-12-05 13:07:50 -0500

    - Humanize clock-in and clock-out times.

--- app/models/clock_session.rb
+++ app/models/clock_session.rb
@@ -1,3 +1,8 @@
 class ClockSession < ApplicationRecord
   belongs_to :user
+
+  def self.formatted_est_datetime(datetime)
+    return unless datetime
+    datetime.in_time_zone("Eastern Time (US & Canada)").to_formatted_s(:long_ordinal) + " EST"
+  end
 end

6ffabfba7b0776ca46b68815d92cfc30b4144818
Andrew Smith <andrewsmith@Andrews-iMac.local>
2018-12-19 23:52:59 -0500

    Minor refactor and guards for finding the total session time of a user.   - Test that checks for a user's ability to grab their total session time over a given time period.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,25 @@ class User < ApplicationRecord
 
   def total_user_session_time_for_date_range(beginning, ending=Date.today)
     user_sessions = clock_sessions_from_date_range(beginning, ending)
-    session_times = user_sessions.collect { |us| us.clock_out - us.created_at }
+
+    session_times = user_sessions.collect do |us|
+      next unless (us.clock_out.present? && us.created_at.present?)
+      us.clock_out - us.created_at
+    end.compact
+
     total_session_time = session_times.inject { |sum, session_time| sum + session_time }
 
     if total_session_time
-      hours, remaining_seconds = total_session_time.divmod(3600)
-      minutes, seconds = remaining_seconds.divmod(60)
-      rounded_seconds = seconds.round(0)
-      "#{hours} #{"hour".pluralize(hours)}, #{minutes} #{"minute".pluralize(minutes)}, #{rounded_seconds} #{"second".pluralize(rounded_seconds)}"
+      format_time(total_session_time)
     else
       "No session times exist for the selected date range."
     end
+  end
+
+  def format_time(total_session_time)
+    hours, remaining_seconds = total_session_time.divmod(3600)
+    minutes, seconds = remaining_seconds.divmod(60)
+    rounded_seconds = seconds.round(0)
+    "#{hours} #{"hour".pluralize(hours)}, #{minutes} #{"minute".pluralize(minutes)}, #{rounded_seconds} #{"second".pluralize(rounded_seconds)}"
   end
 end
--- test/controllers/clock_sessions_controller_test.rb
+++ test/controllers/clock_sessions_controller_test.rb
@@ -6,4 +6,13 @@ class ClockSessionsControllerTest < ActionDispatch::IntegrationTest
     get "/clock_sessions/new"
     assert_response :redirect
   end
+
+  should "allow a user to see their total session time for a given date range" do
+    user = users(:andrew)
+    sign_in users(:andrew)
+    get total_user_clock_sessions_path(user.id, { "begin" => {"year" => "1999", "month" => "11", "day" => "1"}, "end" => {"year" => "2019", "month" => "01", "day" => "01"} })
+    assert_response :success
+    session_time = user.total_user_session_time_for_date_range("1999-01-01", "2001-01-03")
+    assert response.body.match(session_time)
+  end
 end
